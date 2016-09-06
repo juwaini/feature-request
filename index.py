c8e33343fe5bb7656e33d817a847e7b3ae6811bd
juwaini <juwaini@gmail.com>
2016-09-06 21:30:50 +0800

    Add templates and change index routing

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello world!'
+def index():
+    return render_template('index.html')
 
 @app.route('/login')
 def login():
