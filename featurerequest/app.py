7d754b2dec56c52602bf6cc5bef84403bab24504
juwaini <juwaini@gmail.com>
2016-09-15 21:59:15 +0800

    Refactor code to combine get one and get all in one function

--- featurerequest/app.py
+++ featurerequest/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, url_for, render_template, request, jsonify
+from flask_api import status
 from flask_sqlalchemy import SQLAlchemy
 
 from featurerequest.models import Base, FeatureRequest, Client
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite3.db'
@@ -36,59 +39,30 @@ def index():
 def login():
     return 'Please login'
 
-#Route to get whole list
-
-@app.route('/api/feature-requests/', methods=['GET'])
-def feature_requests_list():
-    """Return JSON list of all feature request."""
-    fr = (db.session.query(FeatureRequest).all())
-    json_obj = []
-    for data in fr:
-        json_obj.append({
-            'id': data.id,
-            'title': data.title,
-            'description': data.description
-            })
-    return jsonify(json_obj)
-
-@app.route('/api/clients/', methods=['GET'])
-def clients_list():
-    """Return JSON of all clients"""
-    clients = (db.session.query(Client).all())
-    json_obj = []
-    for data in clients:
-        json_obj.append({
-            'id': data.id,
-            'name': data.name
-            })
-    return jsonify(json_obj)
-
-#Route to get an object based on particular ID
+#Route to get whole list or a particular feature-request
 @app.route('/api/feature-request/<int:feature_request_id>', methods=['GET'])
-def feature_request_get(feature_request_id):
-    """Return JSON of a particular feature request"""
-    fr = db.session.query(FeatureRequest).get(feature_request_id)
-
-    if fr:
-        json_obj = {'id': fr.id,
-                    'title': fr.title,
-                    'description': fr.description}
-        return jsonify(json_obj)
-    else:
-        return 'Id %d not exists.' % feature_request_id, 404
+@app.route('/api/feature-request/', methods=['GET'])
+def get_feature_requests(feature_request_id=None):
+    """Return JSON list of all/particular feature request."""
+    if feature_request_id:
+        fr = db.session.query(FeatureRequest).get(feature_request_id)
+        if not fr:
+            return "Resource not found.", status.HTTP_404_NOT_FOUND
+        return jsonify({'feature-request': fr})
+    fr = (db.session.query(FeatureRequest).all())
+    return jsonify({'feature-request': fr})
 
 @app.route('/api/client/<int:client_id>', methods=['GET'])
-def client_get(client_id):
-    """Return JSON of a particular client"""
-    client = db.session.query(Client).get(client_id)
-
-    if client:
-        json_obj = {'id': client.id,
-                    'name': client.name,
-                    'email': client.email}
-        return jsonify(json_obj)
-    else:
-        return 'Id %d not exists.' % client_id, 404
+@app.route('/api/client/', methods=['GET'])
+def get_clients(client_id=None):
+    """Return JSON of all/particular clients"""
+    if client_id:
+        client = db.session.query(Client).get(client_id)
+        if not client:
+            return "Resource not found.", status.HTTP_404_NOT_FOUND
+        return jsonify({'client': client})
+    client = (db.session.query(Client).all())
+    return jsonify({'client': client})
 
 #Route to create a new object
 @app.route('/api/feature-request/create', methods=['POST'])
